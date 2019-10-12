221a13cce02c2cd740b3b7d08f59640f77bdab1a
PengXiao <eagle.xiao@gmail.com>
2019-10-12 23:02:03 +1100

    Added comment on log

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,14 @@ export default {
   name: 'app',
   mounted() {
     window.App = this;
+    // undefined
     console.log(window.App.$slots.default && window.App.$slots.default[0].text);
     this.$nextTick(() => {
+      // undefined
       console.log(window.App.$slots.default && window.App.$slots.default[0].text)
     });
     setTimeout(() => {
+      // 1234, correct value
       console.log(window.App.$slots.default && window.App.$slots.default[0].text)      
     });
   }
